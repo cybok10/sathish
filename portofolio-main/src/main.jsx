d3a10a612d7fd25141e6f4128f3e4ab5fd355697
cybok <163562827+cybok10@users.noreply.github.com>
2026-09-18 09:51:11 +0530

    Remodel portfolio: simplify app shell

--- portofolio-main/src/main.jsx
+++ portofolio-main/src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
-import PreLoader from "./components/PreLoader.jsx";
 import MentorshipPage from "./components/MentorshipPage.jsx";
 
 const basePath = import.meta.env.BASE_URL.replace(/\/$/, "");
@@ -13,11 +11,6 @@ const isMentorshipPage = currentPath === `${basePath}/mentorship`;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <PreLoader />
-    <div className="container mx-auto px-6">
-      <Navbar />
-      {isMentorshipPage ? <MentorshipPage /> : <App />}
-      <Footer />
-    </div>
-  </StrictMode>,
+    <div><Navbar />{isMentorshipPage ? <MentorshipPage /> : <App />}</div>
+  </StrictMode>
 );
